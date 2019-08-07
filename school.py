6afe8c54eabaf55d6e1a9c4c8bddfcbd081a743f
Isabella Lindgren <isalindgren313@gmail.com>
2019-08-06 22:08:09 -0400

    first attempt Obj Oriented attributes w functions lab

--- school.py
+++ school.py
@@ -1,1 +1,7 @@
-class School:
+class School():
+    def __init__(school, name = None, roster = {}):
+        school.name = name
+        school.roster = roster
+    def add_student(grade, student):
+        school.roster.append(grade, student)
+            
