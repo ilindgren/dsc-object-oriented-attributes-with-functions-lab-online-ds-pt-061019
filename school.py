0cf2a20cce72e9c34fb1b8cbceaad263693cc9b5
Isabella Lindgren <isalindgren313@gmail.com>
2019-08-07 19:28:34 -0400

    completed lab

--- school.py
+++ school.py
@@ -2,6 +2,18 @@ class School():
     def __init__(school, name = None, roster = {}):
         school.name = name
         school.roster = roster
-    def add_student(grade, student):
-        school.roster.append(grade, student)
-            
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+
+        #Alternatively, this could be written as:
+        #self.roster[grade] = self.roster.get(grade, []).append(name)
+    def grade(self, grade):
+           return self.roster[grade]
+    def sort_roster(self):
+            new_dict = self.roster
+            for key in new_dict:
+                new_dict[key].sort()
+            return new_dict
